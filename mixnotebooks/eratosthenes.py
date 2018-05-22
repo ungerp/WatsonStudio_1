efbdabbc077579f70835172c4876c16c9d3c322e
ungerp <patrick.unger@de.ibm.com>
2018-05-22 18:58:08 +0200

    next commit

---
+++ mixnotebooks/eratosthenes.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+def primes_sieve1(limit):
+    limitn = limit+1
+    primes = dict()
+    for i in range(2, limitn): primes[i] = True
+
+    for i in primes:
+        factors = range(i,limitn, i)
+        for f in factors[1:]:
+            primes[f] = False
+    return [i for i in primes if primes[i]==True]
+
